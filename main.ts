54e7d327997a736c6985580f392405da168c8ff3
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-03-16 12:33:45 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,19 @@ input.onGesture(Gesture.TiltLeft, function () {
     Voyager.change(LedSpriteProperty.X, -1)
     Voyager.ifOnEdgeBounce()
 })
+input.onButtonPressed(Button.B, function () {
+    vmiss = game.createSprite(Voyager.get(LedSpriteProperty.X), 4)
+    for (let index = 0; index <= 4; index++) {
+        vmiss.change(LedSpriteProperty.Y, -1)
+        basic.pause(300)
+    }
+    vmiss.delete()
+})
 input.onGesture(Gesture.TiltRight, function () {
     Voyager.change(LedSpriteProperty.X, 1)
     Voyager.ifOnEdgeBounce()
 })
+let vmiss: game.LedSprite = null
 let Voyager: game.LedSprite = null
 Voyager = game.createSprite(2, 4)
 let Kazon = game.createSprite(0, 0)
