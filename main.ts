46a12b038385b2bd430a0cb77d28dccb6cb0913d
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-03-16 18:01:10 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -23,27 +23,30 @@ images.createBigImage(`
     . . . . . . . . . .
     `).scrollImage(1, 200)
 Voyager = game.createSprite(2, 4)
-let Kazon = game.createSprite(1, 0)
+let Kazon = game.createSprite(0, 0)
 game.setLife(3)
 basic.forever(function () {
-    basic.pause(600)
-    if (Kazon.get(LedSpriteProperty.X) < Voyager.get(LedSpriteProperty.X)) {
-        Kazon.set(LedSpriteProperty.X, Kazon.get(LedSpriteProperty.X) + 1)
-    }
-    if (Kazon.get(LedSpriteProperty.X) > Voyager.get(LedSpriteProperty.X)) {
-        Kazon.set(LedSpriteProperty.X, Kazon.get(LedSpriteProperty.X) - 1)
-    }
-    if (randint(0, 10) > 6) {
-        kmiss = game.createSprite(Kazon.get(LedSpriteProperty.X), 0)
-        for (let index = 0; index <= 4; index++) {
-            kmiss.change(LedSpriteProperty.Y, 1)
-            basic.pause(300)
+    basic.pause(2000)
+    while (true) {
+        basic.pause(600)
+        if (Kazon.get(LedSpriteProperty.X) < Voyager.get(LedSpriteProperty.X)) {
+            Kazon.set(LedSpriteProperty.X, Kazon.get(LedSpriteProperty.X) + 1)
         }
-        if (kmiss.get(LedSpriteProperty.X) == Voyager.get(LedSpriteProperty.X)) {
-            game.removeLife(1)
-        } else {
-            game.addScore(50)
+        if (Kazon.get(LedSpriteProperty.X) > Voyager.get(LedSpriteProperty.X)) {
+            Kazon.set(LedSpriteProperty.X, Kazon.get(LedSpriteProperty.X) - 1)
         }
+        if (randint(0, 10) > 6) {
+            kmiss = game.createSprite(Kazon.get(LedSpriteProperty.X), 0)
+            for (let index = 0; index <= 4; index++) {
+                kmiss.change(LedSpriteProperty.Y, 1)
+                basic.pause(300)
+            }
+            if (kmiss.get(LedSpriteProperty.X) == Voyager.get(LedSpriteProperty.X)) {
+                game.removeLife(1)
+            } else {
+                game.addScore(50)
+            }
+        }
+        kmiss.delete()
     }
-    kmiss.delete()
 })
