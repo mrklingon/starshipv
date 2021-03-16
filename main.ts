33d48dfec49ba7e6d0468863629d20942a432112
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-03-16 08:54:33 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,5 +10,11 @@ let Voyager: game.LedSprite = null
 Voyager = game.createSprite(2, 4)
 let Kazon = game.createSprite(0, 0)
 basic.forever(function () {
-    basic.pause(300)
+    basic.pause(600)
+    if (Kazon.get(LedSpriteProperty.X) < Voyager.get(LedSpriteProperty.X)) {
+        Kazon.set(LedSpriteProperty.X, Kazon.get(LedSpriteProperty.X) + 1)
+    }
+    if (Kazon.get(LedSpriteProperty.X) > Voyager.get(LedSpriteProperty.X)) {
+        Kazon.set(LedSpriteProperty.X, Kazon.get(LedSpriteProperty.X) - 1)
+    }
 })
