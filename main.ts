d7f2b0d02b29a337c837b20a868eac3f99e17da5
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-03-16 17:55:19 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,22 +2,29 @@ input.onGesture(Gesture.TiltLeft, function () {
     Voyager.change(LedSpriteProperty.X, -1)
     Voyager.ifOnEdgeBounce()
 })
-input.onButtonPressed(Button.B, function () {
-    vmiss = game.createSprite(Voyager.get(LedSpriteProperty.X), 4)
-    for (let index = 0; index <= 4; index++) {
-        vmiss.change(LedSpriteProperty.Y, -1)
-        basic.pause(300)
-    }
-    vmiss.delete()
-})
 input.onGesture(Gesture.TiltRight, function () {
     Voyager.change(LedSpriteProperty.X, 1)
     Voyager.ifOnEdgeBounce()
 })
-let vmiss: game.LedSprite = null
+let kmiss: game.LedSprite = null
 let Voyager: game.LedSprite = null
+images.createBigImage(`
+    . . . . . . . . . .
+    . # # . . . . . . .
+    # # # # . . # # # .
+    . . # # # # # . . .
+    . . . . . . . . . .
+    `).scrollImage(1, 200)
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . . # . . . .
+    . # . . # # # # # #
+    . . . . . # # # . .
+    . . . . . . . . . .
+    `).scrollImage(1, 200)
 Voyager = game.createSprite(2, 4)
 let Kazon = game.createSprite(0, 0)
+game.setLife(3)
 basic.forever(function () {
     basic.pause(600)
     if (Kazon.get(LedSpriteProperty.X) < Voyager.get(LedSpriteProperty.X)) {
@@ -26,4 +33,17 @@ basic.forever(function () {
     if (Kazon.get(LedSpriteProperty.X) > Voyager.get(LedSpriteProperty.X)) {
         Kazon.set(LedSpriteProperty.X, Kazon.get(LedSpriteProperty.X) - 1)
     }
+    if (randint(0, 10) > 6) {
+        kmiss = game.createSprite(Kazon.get(LedSpriteProperty.X), 0)
+        for (let index = 0; index <= 4; index++) {
+            kmiss.change(LedSpriteProperty.Y, 1)
+            basic.pause(300)
+        }
+        if (kmiss.get(LedSpriteProperty.X) == Voyager.get(LedSpriteProperty.X)) {
+            game.removeLife(1)
+        } else {
+            game.addScore(50)
+        }
+    }
+    kmiss.delete()
 })
