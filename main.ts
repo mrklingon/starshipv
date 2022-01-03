486a4ec1f7cd17b7c5678fb177d1f94358954df1
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2022-01-02 18:14:10 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,17 +10,26 @@ input.onButtonPressed(Button.B, function () {
     Voyager.change(LedSpriteProperty.X, 1)
     Voyager.ifOnEdgeBounce()
 })
+input.onGesture(Gesture.Shake, function () {
+    music.playTone(131, music.beat(BeatFraction.Quarter))
+    Droid += 1
+    if (true) {
+        Droid = 0
+    }
+})
 input.onGesture(Gesture.TiltRight, function () {
     Voyager.change(LedSpriteProperty.X, 1)
     Voyager.ifOnEdgeBounce()
 })
-let kmiss: game.LedSprite = null
 let Resource: game.LedSprite = null
+let kmiss: game.LedSprite = null
+let Droid = 0
 let Voyager: game.LedSprite = null
 Voyager = game.createSprite(2, 4)
 let Kazon = game.createSprite(0, 0)
 game.setLife(3)
 let lives = 3
+Droid = 0
 images.createBigImage(`
     . . . . . . . . . .
     . # # . . . . . . .
@@ -35,30 +44,7 @@ images.createBigImage(`
     . . . . . # # # . .
     . . . . . . . . . .
     `).scrollImage(1, 200)
-basic.forever(function () {
-    basic.pause(5000)
-    while (!(game.isGameOver())) {
-        if (randint(0, 100) > 85) {
-            Resource = game.createSprite(4, randint(0, 4))
-            Resource.turn(Direction.Right, 45)
-            for (let index = 0; index < 40; index++) {
-                Resource.move(1)
-                basic.pause(100)
-                if (Resource.isTouching(Voyager)) {
-                    game.addScore(1000)
-                    game.addLife(1)
-                    lives += 1
-                    if (lives > 2) {
-                        lives = 2
-                        game.setLife(2)
-                    }
-                }
-                Resource.ifOnEdgeBounce()
-            }
-            Resource.delete()
-        }
-    }
-})
+let R_here = 0
 basic.forever(function () {
     basic.pause(2000)
     while (!(game.isGameOver())) {
@@ -86,6 +72,35 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
+    basic.pause(5000)
+    while (!(game.isGameOver())) {
+        if (randint(0, 100) > 85) {
+            Resource = game.createSprite(4, randint(0, 4))
+            R_here = 1
+            Resource.turn(Direction.Right, 45)
+            for (let index = 0; index < 40; index++) {
+                Resource.move(1)
+                if (Droid == 1) {
+                    Voyager.set(LedSpriteProperty.X, Resource.get(LedSpriteProperty.X))
+                }
+                basic.pause(100)
+                if (Resource.isTouching(Voyager)) {
+                    game.addScore(1000)
+                    game.addLife(1)
+                    lives += 1
+                    if (lives > 2) {
+                        lives = 2
+                        game.setLife(2)
+                    }
+                }
+                Resource.ifOnEdgeBounce()
+            }
+            R_here = 0
+            Resource.delete()
+        }
+    }
+})
+basic.forever(function () {
     if (game.score() > 25000) {
         images.createBigImage(`
             . . . . . . . . . .
@@ -96,5 +111,10 @@ basic.forever(function () {
             `).scrollImage(1, 200)
         basic.showString("You have reached the Alpha quadrant!!!")
         game.gameOver()
+    }
+})
+basic.forever(function () {
+    if (Droid == 1 && R_here == 1) {
+        Voyager.set(LedSpriteProperty.X, Resource.get(LedSpriteProperty.X))
     }
 })
